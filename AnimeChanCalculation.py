ad7c58e0e2c4ec4b170d5e12b626586af9339195
Zichen <zichenzz@umich.edu>
2023-04-17 14:44:26 -0400

    specify functions

--- AnimeChanCalculation.py
+++ AnimeChanCalculation.py
@@ -1,6 +1,7 @@
 import sqlite3
+import plotly.graph_objects as go
 
-def calculate_Quote_Percentage(cur,filename):
+def total_char_quote(cur):
     AnimeQuote = []
     AnimeQuoteChar = []
     cur.execute('SELECT UMAnime.Anime, UMAnimeQuotes.Quote FROM UMAnime JOIN UMAnimeQuotes ON UMAnime.id = UMAnimeQuotes.anime_id')
@@ -29,7 +30,10 @@ def calculate_Quote_Percentage(cur,filename):
                 dic[AnimeName]["char_num"][AnimeChar] = 1
             else:
                 dic[AnimeName]["char_num"][AnimeChar] += 1
+    
+    return dic
 
+def percentage_char_quote(dic):
     percentage_dic = {}
     AnimeLst = list(dic.keys())
     for i in range(0,len(AnimeLst)):
@@ -40,7 +44,10 @@ def calculate_Quote_Percentage(cur,filename):
             CharLst = list(dic[AnimeLst[i]]["char_num"].keys())
             for c in CharLst:
                 percentage_dic[AnimeLst[i]]["char_percentage"][c] = round(dic[AnimeLst[i]]["char_num"][c]/percentage_dic[AnimeLst[i]]["quote_num"],2)
-    
+
+    return percentage_dic
+
+def write_result(dic,percentage_dic,filename):
     with open(filename, 'w') as f:
         f.write("Total number of each character's quote in each animation which name contains 'um': "+ str(dic)+"\n")
         f.write("Number of animation which has more than 5 quotes: " + str(len(percentage_dic)) + "\n")
@@ -52,7 +59,9 @@ def main():
     conn = sqlite3.connect('Animeee.db')
     cur = conn.cursor()
     filename = 'AnimeChanCalculation.txt'
-    calculate_Quote_Percentage(cur,filename)
+    dic = total_char_quote(cur)
+    percentage_dic = percentage_char_quote(dic)
+    write_result(dic,percentage_dic,filename)
 
 if __name__ == '__main__':
     main()

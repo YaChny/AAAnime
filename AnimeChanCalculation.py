0d6102e8fde51e7b80693db8e5f2d83129c3ae0b
Zichen <zichenzz@umich.edu>
2023-04-17 14:52:45 -0400

    debug

--- AnimeChanCalculation.py
+++ AnimeChanCalculation.py
@@ -34,12 +34,12 @@ def total_char_quote(cur):
     return dic
 
 def MoreThanFiveQuotes(dic):
-    FiveOrMOre = {}
+    FiveMOre = {}
     AnimeLst = list(dic.keys())
     for i in range(0,len(AnimeLst)):
         if dic[AnimeLst[i]]["quote_num"] > 5:
-            FiveOrMOre[AnimeLst[i]] = dic[AnimeLst[i]]
-    return FiveOrMOre
+            FiveMOre[AnimeLst[i]] = dic[AnimeLst[i]]
+    return FiveMOre
 
 def percentage_char_quote(dic):
     percentage_dic = {}
@@ -55,23 +55,25 @@ def percentage_char_quote(dic):
 
     return percentage_dic
 
-def write_result(FiveOrMore,dic,percentage_dic,filename):
+def write_result(FiveMore,dic,percentage_dic,filename):
     with open(filename, 'w') as f:
         f.write("Total number of each character's quote in each animation which name contains 'um': "+ str(dic)+"\n")
         f.write("Number of animation which has more than 5 quotes: " + str(len(percentage_dic)) + "\n")
-        f.write("Dictionary of animation which has more than 5 quotes: " + str(FiveOrMore) + "\n")
+        f.write("Dictionary of animation which has more than 5 quotes: " + str(FiveMore) + "\n")
         f.write("Percentage of animation which has more than 5 quotes: " + str(round(len(percentage_dic)/len(dic),2)) + "\n")
         f.write("Percentage of each character's quote in each animation which has more than 5 quotes: " + str(percentage_dic) + "\n")
 
+def AnimeChanVisual():
+    pass
 
 def main():
     conn = sqlite3.connect('Animeee.db')
     cur = conn.cursor()
     filename = 'AnimeChanCalculation.txt'
     dic = total_char_quote(cur)
-    FiveOrMore = MoreThanFiveQuotes(dic)
+    FiveMore = MoreThanFiveQuotes(dic)
     percentage_dic = percentage_char_quote(dic)
-    write_result(FiveOrMore,dic,percentage_dic,filename)
+    write_result(FiveMore,dic,percentage_dic,filename)
 
 if __name__ == '__main__':
     main()

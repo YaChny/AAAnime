9abb443a664181ee4594de4a519f5e592c096931
Zichen <zichenzz@umich.edu>
2023-04-17 14:14:38 -0400

    AnimeChanCalculations

---
+++ AnimeChanCalculation.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+def calculate_Quote_Percentage(cur,filename):
+    AnimeQuote = []
+    AnimeQuoteChar = []
+    cur.execute('SELECT UMAnime.Anime, UMAnimeQuotes.Quote FROM UMAnime JOIN UMAnimeQuotes ON UMAnime.id = UMAnimeQuotes.anime_id')
+    for row in cur:
+        AnimeQuote.append(row)
+    cur.execute('SELECT UMAnimeCharacter.Character, UMAnimeQuotes.Quote FROM UMAnimeCharacter JOIN UMAnimeQuotes ON UMAnimeCharacter.id = UMAnimeQuotes.character_id')
+    i = 0
+    for row in cur:
+        newtuple = AnimeQuote[i] + (row[0],)
+        AnimeQuoteChar.append(newtuple)
+        i += 1
+    
+    dic = {}
+    for item in AnimeQuoteChar:
+        AnimeName = item[0]
+        AnimeChar = item[2]
+        if AnimeName not in dic.keys():
+            dic[AnimeName] = {}
+            dic[AnimeName]["quote_num"] = 1
+            dic[AnimeName]["char_num"] = {}
+            dic[AnimeName]["char_num"][AnimeChar] = 1
+
+        else:
+            dic[AnimeName]["quote_num"] += 1
+            if AnimeChar not in dic[AnimeName]["char_num"].keys():
+                dic[AnimeName]["char_num"][AnimeChar] = 1
+            else:
+                dic[AnimeName]["char_num"][AnimeChar] += 1
+
+    percentage_dic = {}
+    AnimeLst = list(dic.keys())
+    for i in range(0,len(AnimeLst)):
+        if dic[AnimeLst[i]]["quote_num"] > 5:
+            percentage_dic[AnimeLst[i]] = {}
+            percentage_dic[AnimeLst[i]]["quote_num"] = dic[AnimeLst[i]]["quote_num"]
+            percentage_dic[AnimeLst[i]]["char_percentage"] = {}
+            CharLst = list(dic[AnimeLst[i]]["char_num"].keys())
+            for c in CharLst:
+                percentage_dic[AnimeLst[i]]["char_percentage"][c] = round(dic[AnimeLst[i]]["char_num"][c]/percentage_dic[AnimeLst[i]]["quote_num"],2)
+    
+    with open(filename, 'w') as f:
+        f.write("Total number of each character's quote in each animation which name contains 'um': "+ str(dic)+"\n")
+        f.write("Number of animation which has more than 5 quotes: " + str(len(percentage_dic)) + "\n")
+        f.write("Percentage of animation which has more than 5 quotes: " + str(round(len(percentage_dic)/len(dic),2)) + "\n")
+        f.write("Percentage of each character's quote in each animation which has more than 5 quotes: " + str(percentage_dic) + "\n")
+
+
+def main():
+    conn = sqlite3.connect('Animeee.db')
+    cur = conn.cursor()
+    filename = 'AnimeChanCalculation.txt'
+    calculate_Quote_Percentage(cur,filename)
+
+if __name__ == '__main__':
+    main()
